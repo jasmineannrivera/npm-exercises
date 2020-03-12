6ee78ea46405ad7aecb5cab13e932d9c69098c85
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-03-12 13:45:31 -0500

    added jquery

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const _ = require('lodash');
-
+const $ = require('jquery');
 const sayHello = () => {
     console.log("hello");
 };
 
+$("#changeColor").on('click', (e) => {
+    $(this).css("background-color", "pink")
+});
 
 
 sayHello();
